cce743e77f50e5b6ad5c0553cc5d99e4dcc4fd0b
rhythmparmar <rhythm0581.be20@chitkara.edu.in>
2023-04-26 08:18:05 +0530

    first commit

---
+++ src/component/Basics/menuApi.js
@@ -0,0 +1,133 @@
+const Menu = [
+  {
+    id: 1,
+    image: "https://assets.teenvogue.com/photos/629e2fab6a33477a16c337b3/master/w_3600,h_2400,c_limit/JENNA-ORTEGA_GALLERY-SINGLE_0287R2C.jpg",
+    name: "Wednesday",
+    category: "Horror-Comedy",
+    description:
+      "Wednesday Addams is expelled from her school after dumping live piranhas into the school's pool in retaliation for the boys' water polo team bullying her brother Pugsley. Consequently, her parents Gomez and Morticia Addams enroll her at their high school alma mater Nevermore Academy, a private school for monstrous outcasts, in the town of Jericho, Vermont. Wednesday's cold, emotionless personality and her defiant nature make it difficult for her to connect with her schoolmates and cause her to run afoul of the school's principal Larissa Weems.",
+  },
+
+  {
+    id: 2,
+    image: "https://m.media-amazon.com/images/M/MV5BNDE2ODVmNGMtOGI3Zi00ODNjLTg5ZmUtNzAxNjQ4M2FjNzlkXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_.jpg",
+    name: "The Man from Toronto",
+    category: "Action-Comedy",
+    description:
+      "Teddy, a struggling fitness entrepreneur in Yorktown, Virginia, is fired from his job at a local gym. He decides not to tell his wife Lori, taking her to Onancock for her birthday. Leaving her at a spa, Teddy arrives at the wrong cabin, where a man named Coughlin is being held hostage. Mistaken for the Man from Toronto, a mysterious assassin with a talent for brutal interrogation, the clueless Teddy manages to intimidate Coughlin into giving up a code. ",
+  },
+  {
+    id: 3,
+    image: "https://m.media-amazon.com/images/M/MV5BNTZjNzRjMTMtZDMzNy00Y2ZjLTg0OTAtZjVhNzYyZmJjOTljXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_.jpg",
+    name: "21st Jump street",
+    category: "Action-Comedy",
+    description:
+      "In 2005, scholarly student Morton Schmidt and popular yet underachieving athlete Greg Jenko miss their school prom, Schmidt being rejected by the girl he was trying to ask to be his date and Jenko being barred from attending due to failing grades. Seven years later, the duo meets again at the police academy and become friends and partners on bicycle patrol. ",
+  },
+  {
+    id: 4,
+    image: "https://flxt.tmsimg.com/assets/p9742380_p_v8_ad.jpg",
+    name: "We are the Millers",
+    category: "Action-Comedy",
+    description:
+      "A low-level marijuana dealer living in Denver, David Clark, is robbed of his money and stash, some of which he owes to his drug supplier, Brad Gurdlinger. Gurdlinger forces him to smuggle marijuana from Mexico to clear his debt. ",
+  },
+  {
+    id: 5,
+    image: "https://m.media-amazon.com/images/M/MV5BMTM2MTM4MzY2OV5BMl5BanBnXkFtZTcwNjQ3NzI4NA@@._V1_.jpg",
+    name: "Hangover",
+    category: "Action-Thriller",
+    description:
+      "Two years after the events in Las Vegas, Stu Price travels to Thailand for his upcoming wedding to Lauren, his fiancée, along with his three best friends, Doug Billings, Phil Wenneck and Alan Garner. Much to Alan's dismay, they are joined by Lauren's younger brother, a Stanford University scholar named Teddy. At the rehearsal dinner, Lauren's father reveals his disapproval of Stu during a toast. Later that night, Stu hesitantly joins Phil, Doug, Alan and Teddy for a beer.",
+  },
+  {
+    id: 6,
+    image: "https://m.media-amazon.com/images/M/MV5BYjhiNjBlODctY2ZiOC00YjVlLWFlNzAtNTVhNzM1YjI1NzMxXkEyXkFqcGdeQXVyMjQxNTE1MDA@._V1_FMjpg_UX1000_.jpg",
+    name: "Avatar",
+    category: "Action",
+    description:
+      "Sixteen years after the Na'vi repelled the RDA invasion of Pandora,[a] Jake Sully lives as chief of the Omatikaya clan and raises a family with Neytiri, which includes sons Neteyam and Lo'ak, daughter Tuk, and adopted children Kiri (born from Grace Augustine's inert avatar) and Spider, the Pandora-born human son of the late Colonel Miles Quaritch. To the Na'vi's dismay, the RDA, led by their new leader Frances Ardmore, returns to colonize Pandora, as Earth is dying. ",
+  },
+  {
+    id: 7,
+    image: "https://m.media-amazon.com/images/M/MV5BZDZkZTg2YmQtNDYxMy00MDE4LTlmZWItNzMwZjkyYTk3NWMzXkEyXkFqcGdeQXVyMTA1OTAyOTI@._V1_.jpg",
+    name: "Guns Akimbo",
+    category: "Action",
+    description:
+      "In an alternative near future, an underground fight club and a criminal organization known as Skizm has achieved massive popularity by live-streaming real death matches between criminals and psychos. Ordinary computer programmer Miles Lee Harris, who gets his kicks by trolling online trolls, logs into Skizm’s forum to insult viewers who turn murder into entertainment. Riktor, the criminal kingpin and psychopath who runs Skizm, breaks into Miles’ apartment with his henchmen Dane, Effie, and Fuckface.",
+  },
+  {
+    id: 8,
+    image: "https://m.media-amazon.com/images/M/MV5BN2FkOWQ3YzItNmNhZi00ZWNlLThjYTMtZWIyZDc2YjQzMjk3XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg",
+    name: "transformers - Rise of the Beasts",
+    category: "Action",
+    description:
+      "In the year 1994, a pair of human archaeologists from Brooklyn come into an ancient conflict through a globe-trotting adventure with the Autobots that ties in with three factions of the Transformers race: the Maximals, the Predacons and the Terrorcons. ",
+  },
+  {
+    id: 9,
+    image: "https://images-na.ssl-images-amazon.com/images/S/pv-target-images/f5a21f64af7359f7aaa7c29dee8a12a97630e707cdbf71e0ae6b063322fb8575._RI_V_TTW_.jpg",
+    name: "KGF 2",
+    category: "Action",
+    description:
+      "After detailing the events in K.G.F: Chapter 1, Anand Ingalagi suffers a stroke and his son, Vijayendra Ingalagi takes over to narrate the rest of the story. Rocky kills heir apparent Virat and takes over the K.G.F, keeping Reena hostage to ensure the cooperation of Guru Pandian, Andrews, Rajendra Desai and Kamal. But when Rocky shows his arrogance and demands to take Reena as his wife, Reena's brother Kamal got enraged and threatenes Rocky. This leads Rocky to lose his patience and kills Kamal on the spot. After this, Rocky takes Reena to his palace and issues orders to start work in eight hidden mines, While Vanaram, who was captured by Rocky, decided to help him. ",
+  },
+  {
+    id: 10,
+    image: "https://upload.wikimedia.org/wikipedia/en/d/d0/John_Wick_-_Chapter_4_promotional_poster.jpg",
+    name: "John Wick 4",
+    category: "Action-Thriller",
+    description:
+      "In New York City, John Wick prepares to exact revenge against the High Table while hiding underground with the Bowery King. He travels to Morocco and kills the Elder, the one who sits above the Table. In response, the Marquis Vincent de Gramont, a member of the High Table, summons New York Continental hotel manager Winston and his concierge, Charon.",
+  },
+  {
+    id: 11,
+    image: "https://upload.wikimedia.org/wikipedia/en/f/ff/The_Batman_%28film%29_poster.jpg",
+    name: "The Bat Man",
+    category: "Action-Thriller",
+    description:
+      "On Halloween, Gotham City mayor Don Mitchell Jr. is murdered by the Riddler, a masked serial killer. Reclusive billionaire Bruce Wayne, who has operated for two years as the vigilante Batman, investigates the murder alongside the Gotham City Police Department (GCPD). Lieutenant James Gordon discovers a message that the Riddler left for Batman.",
+  },
+  {
+    id: 12,
+    image: "https://upload.wikimedia.org/wikipedia/en/f/f2/Fast_X_poster.jpg",
+    name: "The Fast X",
+    category: "Action-Thriller",
+    description:
+      "Fast X[b] (also known as Fast & Furious 10) is an upcoming American action film directed by Louis Leterrier from a screenplay by Justin Lin, Zach Dean, and Dan Mazeau. It is the sequel to F9 (2021), serving as the tenth and penultimate main installment, and the eleventh full-length film in the Fast & Furious franchise. ",
+  },
+  {
+    id: 13,
+    image: "https://upload.wikimedia.org/wikipedia/en/b/b1/Poster_Renfield.jpg",
+    name: "Renfield",
+    category: "Horror-Comedy",
+    description:
+      "Renfield is a 2023 American comedy horror film[4] directed by Chris McKay from a screenplay by Ryan Ridley, based on an original idea by Robert Kirkman featuring characters from Bram Stoker's 1897 novel Dracula. The film stars Nicholas Hoult as the title character, alongside Nicolas Cage as Count Dracula, with Awkwafina, Ben Schwartz, and Adrian Martinez in supporting roles. ",
+  },
+  {
+    id: 14,
+    image: "https://upload.wikimedia.org/wikipedia/en/2/24/Scream2022film.jpeg",
+    name: "Scream V",
+    category: "Horror-Comedy",
+    description:
+      "In Modesto, Tara's estranged older sister Sam Carpenter is informed by Wes Hicks, one of Tara's friends, about the attack. Sam returns to Woodsboro with her boyfriend Richie Kirsch to visit Tara at the hospital, where Sam is reunited with Tara's friend group: Wes, Amber Freeman, twins Chad and Mindy Meeks-Martin, and Liv McKenzie. That night, Liv's ex-boyfriend Vince Schneider, who is Stu's nephew, is killed by Ghostface.",
+  },
+  {
+    id: 15,
+    image: "https://upload.wikimedia.org/wikipedia/en/1/14/The_Boys_Season_2.jpg",
+    name: "The Boyz",
+    category: "Horror-Comedy",
+    description:
+      "The Boys is an American superhero television series developed by Eric Kripke for Amazon Prime Video. Based on the comic book of the same name by Garth Ennis and Darick Robertson, it follows the eponymous team of vigilantes as they combat superpowered individuals who abuse their abilities. The series features an ensemble cast that includes Karl Urban, Jack Quaid, Antony Starr, Erin Moriarty, Dominique McElligott, Jessie T. Usher, Chace Crawford, Laz Alonso, Tomer Capone, Karen Fukuhara, and Nathan Mitchell. ",
+  },
+  {
+    id: 16,
+    image: "https://upload.wikimedia.org/wikipedia/en/4/46/Army_of_Darkness_poster.jpg",
+    name: "Army of Darkness",
+    category: "Horror-Comedy",
+    description:
+    "Army of Darkness[a] is a 1992 American dark fantasy horror comedy film directed, co-written, and co-edited (under the pseudonym R. O. C. Sandstorm) by Sam Raimi.[3][4][5] The film is the third installment in the Evil Dead film series and the sequel to Evil Dead II (1987). Starring Bruce Campbell and Embeth Davidtz, it follows Ash Williams (Campbell) as he is trapped in the Middle Ages and battles the undead in his quest to return to the present.",
+  },
+];
+
+export default Menu;
---
+++ src/component/Navbar2.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar2 = () => {
+  return (
+    <>
+          <nav className='nav2'>
+               <img src='https://www.edigitalagency.com.au/wp-content/uploads/Netflix-logo-red-black-png.png' alt='Netflix'/>
+          </nav>
+    </>
+  )
+}
+
+export default Navbar2
